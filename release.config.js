0fdbf7f5bd7fb1d940987e52613393d947cf8613
Zarif Aziz <zarif.aziz@faethm.ai>
2023-10-18 13:36:33 +1100

    fix: updated release config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const config = {
     branches: ['main'],
     plugins: [
-        'semantic-release/commit-analyzer',
+        '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
             "assets": ["dist/*.js", "dist/*.js.map"],
